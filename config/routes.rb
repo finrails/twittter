d9ea476baf314d944674599e4d8f13c3aa8d7592
finrails <macacofinn@yahoo.com>
2022-05-05 21:34:35 -0400

    add: user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweeets
+  resources :users, except: %i[ index create new  ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
