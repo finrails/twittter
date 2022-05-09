c89427e357d51af79ef4b7cf0bce5de3c4b7adf4
finrails <macacofinn@yahoo.com>
2022-05-09 13:41:56 -0400

    add: 404 error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweeets
-  resources :users, except: %i[ index create new  ]
+  resources :users, except: %i[ show index create new  ]
 
+  get 'users/:username', to: 'users#show', as: :show_user
+
+  get '/404', to: 'errors#not_found', as: :not_found
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
